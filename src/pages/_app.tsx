e71f44c163967205a60be5a674a6d255ea1d0319
Cornelius Denninger <cornelius.denninger@gmail.com>
2021-08-10 14:48:23 +0200

    added google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,22 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>XRP to the Moon</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
       </Head>
       <Component {...pageProps} />
     </>
